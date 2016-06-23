90010dc2659db45a0a2343d432078ac5361da1f0
Jeremy Hanson-Finger <jeremy.hansonfinger@shopify.com>
2016-06-23 16:58:49 -0400

    Format ruleset as array of hashes

--- spec/erb_lint/linters/content_style_checker_spec.rb
+++ spec/erb_lint/linters/content_style_checker_spec.rb
@@ -1,13 +1,12 @@
 # frozen_string_literal: true
 
 require 'spec_helper'
-require 'YAML'
 
 describe ERBLint::Linter::ContentStyleChecker do
   let(:linter_config) do
     {
       'rule_set' => rule_set,
-      'addendum' => 'Questions? Contact #product-content on Slack or @Shopify/product-content on GitHub.'
+      'addendum' => 'Questions? Contact Lintercorp Product Content at product-content@lintercorp.com.'
     }
   end
 
@@ -15,101 +14,46 @@ describe ERBLint::Linter::ContentStyleChecker do
 
   subject(:linter_errors) { linter.lint_file(ERBLint::Parser.parse(file)) }
 
-  let(:rules) { <<~FILE }
-  ---
-
-  - violation:
-      - 'dropdown'
-      - 'drop down'
-    case_insensitive: true
-    suggestion: "drop-down"
-
-  - violation:
-      - 'Shopify partner'
-    suggestion: 'Shopify Partner'
-
-  - violation:
-      - 'Shopify partners'
-    suggestion: 'Shopify Partners'
-
-  - violation:
-      - 'manual'
-      - 'Shopify manual'
-      - 'docs'
-      - 'documentation'
-      - 'support docs'
-    case_insensitive: true
-    suggestion: 'Shopify Help Center'
-
-  - violation:
-      - 'Help Center'
-      - 'help center'
-    suggestion: 'Shopify Help Center'
-
-  - violation:
-      - 'theme store'
-      - 'Theme Store'
-    suggestion: 'Shopify theme store'
-
-  - violation: 'Theme'
-    suggestion: 'theme'
-
-  - violation: 'Themes'
-    suggestion: 'themes'
-
-  - violation:
-      - 'store’s admin'
-      - "store's admin"
-      - 'backend store admin'
-      - 'back-end store admin'
-      - 'admin'
-    case_insensitive: true
-    suggestion: 'Shopify admin'
-
-  - violation: 'Shopify Admin'
-    suggestion: 'Shopify admin'
-
-  - violation: 'Store'
-    suggestion: 'store'
-
-  - violation: 'Stores'
-    suggestion: 'stores'
-
-  - violation:
-      - 'application'
-      - 'applications'
-    case_insensitive: true
-    suggestion: 'app'
-
-  - violation:
-      - 'applications'
-    case_insensitive: true
-    suggestion: 'apps'
-
-  - violation:
-      - 'App'
-    suggestion: 'app'
-
-  - violation:
-      - 'Apps'
-    suggestion: 'apps'
-
-  - violation:
-    - 'Payment Gateway'
-    - 'payment-gateway'
-    suggestion: 'payment gateway'
-
-  - violation:
-      - 'Payment Gateways'
-      - 'payment-gateways'
-    suggestion: 'payment gateways'
-
-    FILE
-
   let(:rule_set) do
-    # require 'pry'
-    # binding.pry
-    YAML.load(rules)
+    [
+      { 'violation' => ['dropdown', 'drop down'],
+        'case_insensitive' => true,
+        'suggestion' => 'drop-down' },
+      { 'violation' => ['Lintercorp partner'], 'suggestion' => 'Lintercorp Partner' },
+      { 'violation' => ['Lintercorp partners'], 'suggestion' => 'Lintercorp Partners' },
+      { 'violation' =>
+       ['manual', 'Lintercorp manual', 'docs', 'documentation', 'support docs'],
+        'case_insensitive' => true,
+        'suggestion' => 'Lintercorp Help Center' },
+      { 'violation' => ['Help Center', 'help center'],
+        'suggestion' => 'Lintercorp Help Center' },
+      { 'violation' => ['theme store', 'Theme Store'],
+        'suggestion' => 'Lintercorp theme store' },
+      { 'violation' => 'Theme', 'suggestion' => 'theme' },
+      { 'violation' => 'Themes', 'suggestion' => 'themes' },
+      { 'violation' =>
+       ['store’s dashboard',
+        'store\'s dashboard',
+        'backend store dashboard',
+        'back-end store dashboard',
+        'dashboard'],
+        'case_insensitive' => true,
+        'suggestion' => 'Lintercorp dashboard' },
+      { 'violation' => 'Lintercorp Dashboard', 'suggestion' => 'Lintercorp dashboard' },
+      { 'violation' => 'Store', 'suggestion' => 'store' },
+      { 'violation' => 'Stores', 'suggestion' => 'stores' },
+      { 'violation' => ['application', 'applications'],
+        'case_insensitive' => true,
+        'suggestion' => 'app' },
+      { 'violation' => ['applications'],
+        'case_insensitive' => true,
+        'suggestion' => 'apps' },
+      { 'violation' => ['App'], 'suggestion' => 'app' },
+      { 'violation' => ['Apps'], 'suggestion' => 'apps' },
+      { 'violation' => ['Payment Gateway', 'payment-gateway'],
+        'suggestion' => 'payment gateway' },
+      { 'violation' => ['Payment Gateways'], 'suggestion' => 'payment gateways' }
+    ]
   end
 
   context 'when the rule set is empty' do
@@ -129,6 +73,7 @@ describe ERBLint::Linter::ContentStyleChecker do
     - file has violation with different case (`Drop down`)
     - file has violation with same case (`dropdown`)
     - file has suggestion (`drop-down`)' do
+
       let(:file) { <<~FILE }
         <p>Tune in, turn on, and drop-down out! And check out the Drop down and dropdown menu too.</p>
 
@@ -146,11 +91,11 @@ describe ERBLint::Linter::ContentStyleChecker do
       end
     end
 
-    context '- suggestion is prefix + violation (`Shopify Help Center`)
-    - file contains suggestion (`Shopify Help Center`)
+    context '- suggestion is prefix + violation (`Lintercorp Help Center`)
+    - file contains suggestion (`Lintercorp Help Center`)
     - file contains violation (`Help Center`)' do
       let(:file) { <<~FILE }
-        <p>Help! I need a Shopify Help Center. Not just any Help Center. Help!</p>
+        <p>Help! I need a Lintercorp Help Center. Not just any Help Center. Help!</p>
 
       FILE
 
@@ -158,16 +103,16 @@ describe ERBLint::Linter::ContentStyleChecker do
         expect(linter_errors.size).to eq 1
       end
 
-      it 'reports error for `Help Center` and suggests `Shopify Help Center`' do
+      it 'reports error for `Help Center` and suggests `Lintercorp Help Center`' do
         expect(linter_errors[0][:message]).to include 'Don\'t use `Help Center`'
-        expect(linter_errors[0][:message]).to include 'Do use `Shopify Help Center`'
+        expect(linter_errors[0][:message]).to include 'Do use `Lintercorp Help Center`'
       end
     end
 
-    context '- file contains suggestion (`Shopify Help Center`)
+    context '- file contains suggestion (`Lintercorp Help Center`)
     - file contains violation (`help center`)' do
       let(:file) { <<~FILE }
-        <p>Help. I need a Shopify Help Center. Not just any help center. Help.</p>
+        <p>Help. I need a Lintercorp Help Center. Not just any help center. Help.</p>
 
       FILE
 
@@ -175,13 +120,13 @@ describe ERBLint::Linter::ContentStyleChecker do
         expect(linter_errors.size).to eq 1
       end
 
-      it 'reports error for `help center` and suggests `Shopify Help Center`' do
+      it 'reports error for `help center` and suggests `Lintercorp Help Center`' do
         expect(linter_errors[0][:message]).to include 'Don\'t use `help center`'
-        expect(linter_errors[0][:message]).to include 'Do use `Shopify Help Center`'
+        expect(linter_errors[0][:message]).to include 'Do use `Lintercorp Help Center`'
       end
     end
 
-    context '- suggestion is prefix + violation (`Shopify theme store`)
+    context '- suggestion is prefix + violation (`Lintercorp theme store`)
     - file contains violation (`theme store`)
     - file contains violation (`Theme store`)
     - violation contained in prior violation (`Theme`)' do
@@ -194,11 +139,11 @@ describe ERBLint::Linter::ContentStyleChecker do
         expect(linter_errors.size).to eq 2
       end
 
-      it 'reports errors for `theme store` and `Theme Store` and suggests `Shopify theme store`' do
+      it 'reports errors for `theme store` and `Theme Store` and suggests `Lintercorp theme store`' do
         expect(linter_errors[0][:message]).to include 'Don\'t use `theme store`'
-        expect(linter_errors[0][:message]).to include 'Do use `Shopify theme store`'
+        expect(linter_errors[0][:message]).to include 'Do use `Lintercorp theme store`'
         expect(linter_errors[1][:message]).to include 'Don\'t use `Theme Store`'
-        expect(linter_errors[1][:message]).to include 'Do use `Shopify theme store`'
+        expect(linter_errors[1][:message]).to include 'Do use `Lintercorp theme store`'
       end
     end
 
@@ -256,69 +201,65 @@ describe ERBLint::Linter::ContentStyleChecker do
       end
     end
 
-    context '- violation has multiple words and first word starts with uppercase character (`Shopify partner`)
-    - suggestion has multiple words, both starting with uppercase characters (`Shopify Partner`)' do
+    context '- violation has multiple words and first word starts with uppercase character (`Lintercorp partner`)
+    - suggestion has multiple words, both starting with uppercase characters (`Lintercorp Partner`)' do
       let(:file) { <<~FILE }
-        <p>Are you a Shopify partner, partner?</p>
+        <p>Are you a Lintercorp partner, partner?</p>
       FILE
 
       it 'reports 1 errors' do
         expect(linter_errors.size).to eq 1
       end
 
-      it 'reports errors for `Shopify partner` and suggests `Shopify Partner`' do
-        expect(linter_errors[0][:message]).to include 'Don\'t use `Shopify partner`'
-        expect(linter_errors[0][:message]).to include 'Do use `Shopify Partner`'
+      it 'reports errors for `Lintercorp partner` and suggests `Lintercorp Partner`' do
+        expect(linter_errors[0][:message]).to include 'Don\'t use `Lintercorp partner`'
+        expect(linter_errors[0][:message]).to include 'Do use `Lintercorp Partner`'
       end
     end
 
-    context '- violation has a single dumb quote (`Store\'s admin`)' do
+    context '- violation has a single dumb quote (`Store\'s dashboard`)' do
       let(:file) { <<~FILE }
-        <p>Does the Store's admin add Finn?</p>
+        <p>Welcome to the Store's dashboard.</p>
       FILE
 
       it 'reports 1 errors' do
         expect(linter_errors.size).to eq 1
       end
 
-      it 'reports errors for `store\'s admin` and suggests `Shopify admin`' do
-        expect(linter_errors[0][:message]).to include 'Don\'t use `store\'s admin`'
-        expect(linter_errors[0][:message]).to include 'Do use `Shopify admin`'
+      it 'reports errors for `store\'s dashboard` and suggests `Lintercorp dashboard`' do
+        expect(linter_errors[0][:message]).to include 'Don\'t use `store\'s dashboard`'
+        expect(linter_errors[0][:message]).to include 'Do use `Lintercorp dashboard`'
       end
     end
 
-    context '- violation has a single smart quote (`Store’s admin`)
+    context '- violation has a single smart quote (`Store’s dashboard`)
     - violation contained in prior violation' do
       let(:file) { <<~FILE }
-        <p>Is the Store’s admin named Finn?</p>
+        <p>Welcome to the Store’s dashboard.</p>
       FILE
 
       it 'reports 1 errors' do
         expect(linter_errors.size).to eq 1
       end
 
-      # TODO: Capture the real violation and push that instead of the violated rule?
-
-      it 'reports errors for `store’s admin` and suggests `Shopify admin`' do
-        expect(linter_errors[0][:message]).to include 'Don\'t use `store’s admin`'
-        expect(linter_errors[0][:message]).to include 'Do use `Shopify admin`'
+      it 'reports errors for `store’s dashboard` and suggests `Lintercorp dashboard`' do
+        expect(linter_errors[0][:message]).to include 'Don\'t use `store’s dashboard`'
+        expect(linter_errors[0][:message]).to include 'Do use `Lintercorp dashboard`'
       end
     end
 
-    context '- file has two double dumb quotes (`"backend store admin"`)' do
+    context '- file has two double dumb quotes (`"backend store dashboard"`)' do
       let(:file) { <<~FILE }
-        <p>Does the "backend store admin" add Finn?</p>
+        <p>Welcome to the "backend store dashboard".</p>
       FILE
 
       it 'reports 1 errors' do
         expect(linter_errors.size).to eq 1
       end
 
-      # TODO: I also need to make it scan the entire rule worth of violations so it doesn't have to go in order.
-
-      it 'reports errors for `backend store admin` and suggests `Shopify admin`' do
-        expect(linter_errors[0][:message]).to include 'Don\'t use `backend store admin`'
-        expect(linter_errors[0][:message]).to include 'Do use `Shopify admin`'
+      it 'reports errors for `backend store dashboard` and suggests `Lintercorp dashboard`' do
+        expect(linter_errors[0][:message]).to include 'Don\'t use `backend store dashboard`'
+        expect(linter_errors[0][:message]).to include 'Do use `Lintercorp dashboard`'
       end
     end
 
